545fbc782a5762fa6dc4ad53287d86c372debe76
atulpandey2026 <atul@thefoundermedia.in>
2026-09-17 15:33:29 +0530

    Update UtilityBar.tsx

--- src/app/components/UtilityBar.tsx
+++ src/app/components/UtilityBar.tsx
@@ -19,8 +19,8 @@ export default function UtilityBar() {
           <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" className="text-white">
             <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 12a19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 3.6 1.18h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L7.91 8.91a16 16 0 0 0 6 6l.91-.91a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z" />
           </svg>
-          <a href="tel:+917982032246" className="text-xs font-700 tracking-wide whitespace-nowrap hover:underline text-white">
-            +91 79820 32246
+          <a href="tel:+9188820 02378" className="text-xs font-700 tracking-wide whitespace-nowrap hover:underline text-white">
+            +9188820 02378
           </a>
         </div>
 
